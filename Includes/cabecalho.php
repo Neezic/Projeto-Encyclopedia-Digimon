bc1f6a4a0d62c0b458f1fffbcdb555d8ec350f47
João Lucas Sorda de Almeida <joaolucasdealmeida1@gmail.com>
2025-04-12 18:24:49 -0300

    icones de bootstrap
    icones do bootstrap adicionados.

--- Includes/cabecalho.php
+++ Includes/cabecalho.php
@@ -10,6 +10,7 @@ session_start();
     
     <!-- Bootstrap CSS (opcional) -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     
     <!-- CSS Personalizado -->
     <style>
