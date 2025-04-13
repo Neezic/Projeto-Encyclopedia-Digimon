f36b8041f8c4801831e05fe427c31c1c17bf0725
João Lucas Sorda de Almeida <joaolucasdealmeida1@gmail.com>
2025-04-13 17:32:58 -0300

    Mudanças de estilização
    mudanças da estilização da página

--- Includes/cabecalho.php
+++ Includes/cabecalho.php
@@ -11,7 +11,7 @@ session_start();
     <!-- Bootstrap CSS (opcional) -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    
+    <link href="https://fonts.cdnfonts.com/css/digimon" rel="stylesheet">
     <!-- CSS Personalizado -->
     <style>
         body {
